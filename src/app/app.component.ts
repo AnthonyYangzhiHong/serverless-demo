ee61ac800a4aef2832c079252efd021180353072
Yangzhi Hong <anthonyhong9011@gmail.com>
2024-10-24 14:48:51 -0400

    add cognito auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
+import AmplifyAuthenticator, { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TodosComponent],
+  imports: [RouterOutlet, TodosComponent, AmplifyAuthenticatorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
